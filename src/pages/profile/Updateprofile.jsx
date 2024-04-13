be25c0a5884933def47abec806356538ba05fdcb
saurav-saksena <sauravkumar4363@gmail.com>
2024-04-13 08:24:50 +0530

    small changes

--- src/pages/profile/Updateprofile.jsx
+++ src/pages/profile/Updateprofile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import "./profile.css"
 import SocialContext from '../../ContextStore/SocialContext'
 
@@ -53,45 +53,48 @@ export default function Updateprofile() {
         }
     })
     return (
-        <div className='update--container'>
-            <h1>Update profile</h1>
-            <div className='update--item'>
-                <span id='notify'>username can not be changed</span>
-                <input type='text' disabled name='username' onChange={handleChange} value={userData.username} />
-            </div>
-            <div className='update--item'>
-                <label htmlFor='desc1'> description :</label>
-                <input type='text' id='desc1' name='desc' onChange={handleChange} value={userData.desc} />
-            </div>
-            <div className='update--item'>
-                <label htmlFor='city1'> city :  </label>
-                <input type='text' id='city1' name='city' onChange={handleChange} value={userData.city} />
+        <>
+            <Link className='return--button' to="/">home</Link>
+            <div className='update--container'>
+                <h1>Update profile</h1>
+                <div className='update--item'>
+                    <span id='notify'>username can not be changed</span>
+                    <input type='text' disabled name='username' onChange={handleChange} value={userData.username} />
+                </div>
+                <div className='update--item'>
+                    <label htmlFor='desc1'> description :</label>
+                    <input type='text' id='desc1' name='desc' onChange={handleChange} value={userData.desc} />
+                </div>
+                <div className='update--item'>
+                    <label htmlFor='city1'> city :  </label>
+                    <input type='text' id='city1' name='city' onChange={handleChange} value={userData.city} />
 
-            </div>
-            <div className='update--item'>
-                <label htmlFor='from1'> from :  </label>
-                <input type='text' name='from' onChange={handleChange} value={userData.from} />
+                </div>
+                <div className='update--item'>
+                    <label htmlFor='from1'> from :  </label>
+                    <input type='text' name='from' onChange={handleChange} value={userData.from} />
 
+                </div>
+                <div className='update--item'>
+                    <label>relationship</label>
+                    <select name='relationship' onChange={handleChange} value={userData.relationship}>
+                        <option value="">choose relationship</option>
+                        <option value="single">single</option>
+                        <option value="married">married</option>
+                        <option value="divorced">divorced</option>
+                        <option value="don't disclose">other</option>
+                    </select>
+                </div>
+                <div className='update--item'>
+                    <label>profile picture</label>
+                    <input type='file' onChange={handleFile} name='profilePicture' />
+                </div>
+                <div className='update--item'>
+                    <label>cover picture</label>
+                    <input type='file' onChange={handleFile} name='coverPicture' />
+                </div>
+                <button onClick={updateButton} className='update--button'>update_profile</button>
             </div>
-            <div className='update--item'>
-                <label>relationship</label>
-                <select name='relationship' onChange={handleChange} value={userData.relationship}>
-                    <option value="">choose relationship</option>
-                    <option value="single">single</option>
-                    <option value="married">married</option>
-                    <option value="divorced">divorced</option>
-                    <option value="don't disclose">other</option>
-                </select>
-            </div>
-            <div className='update--item'>
-                <label>profile picture</label>
-                <input type='file' onChange={handleFile} name='profilePicture' />
-            </div>
-            <div className='update--item'>
-                <label>cover picture</label>
-                <input type='file' onChange={handleFile} name='coverPicture' />
-            </div>
-            <button onClick={updateButton} className='update--button'>update_profile</button>
-        </div>
+        </>
     )
 }
